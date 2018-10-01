bddf4914187394a9b5c2114c4a3d2628dee545f1
Petre Vane <petre.vane@gmail.com>
2018-10-01 18:40:07 +0200

    Added cell checkmark

--- ToDoey/ToDoey/ToDoViewController.swift
+++ ToDoey/ToDoey/ToDoViewController.swift
@@ -10,37 +10,68 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
+    // MARK: - Outlets
+    
+    var listElements = ["Grab a beer", "Grab a towel", "Wash your beard"]
+    
+    //@IBOutlet var tableView: UITableView! --> this is not needed when having an UITableViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        }
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return listElements.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
 
         // Configure the cell...
+        
+        cell.textLabel?.text = listElements[indexPath.row]
 
         return cell
     }
-    */
+    
+    
+    // tapping into table properties
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // making the cell flash one time when selected
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // printing the content of the cells
+        
+        print("Your cell content is: \(listElements[indexPath.row])")
+        
+        // Adding a checkmark for each cell
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    
+        
+        
+    }
+    
+    
+    
 
     /*
     // Override to support conditional editing of the table view.
