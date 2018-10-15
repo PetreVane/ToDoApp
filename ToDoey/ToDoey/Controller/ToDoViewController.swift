2f36c60876a6eb8901130b04da4fb61276ebe7da
Petre Vane <petre.vane@gmail.com>
2018-10-15 15:30:28 +0200

    Added database query (SearchBar text field with NSPredicate & sortDescriptor)

--- ToDoey/ToDoey/Controller/ToDoViewController.swift
+++ ToDoey/ToDoey/Controller/ToDoViewController.swift
@@ -128,7 +128,7 @@ class ToDoViewController: UITableViewController {
     
     
         
-    func loadSavedData(){
+    func loadSavedData(with request: NSFetchRequest<Item> = Item.fetchRequest()){
         
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         
@@ -137,11 +137,34 @@ class ToDoViewController: UITableViewController {
         } catch {
             print("Your fetching error is: \(error)")
         }
-       
+        tableView.reloadData()
         }
 
     }
+
+
+extension ToDoViewController: UISearchBarDelegate {
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS [cd] %@", searchBar.text!)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        
+        request.predicate = predicate
+        request.sortDescriptors = [sortDescriptor]
+        
+        do {
+            listElements = try context.fetch(request)
+        } catch {
+            print("Your fetching error is: \(error)")
+        }
+        
+        
+    }
+    
+    
+}
   
         
 
